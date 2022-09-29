4bbd6cc95f6a7fbc4715212cdd1920d9f5133150
Abby Nyakara <abbynyakara@gmail.com>
2022-09-29 13:37:10 +0300

    add associations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
-# rubocop:disable
-
 class Comment < ApplicationRecord
+  belongs_to :user
+  belongs_to :post
+
+  def comments_counter
+    post.increment!(:comments_counter)
+  end
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,12 @@
-# rubocop:disable
-
 class Like < ApplicationRecord
+  belongs_to :user
+  belongs_to :post
+
+  after_save :likes_counter
+
+  def likes_counter
+    post.increment!(:post_counter)
+  end
+
+  private :likes_counter
 end
