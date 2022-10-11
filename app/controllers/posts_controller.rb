53d84a792aea63a00a638a48475c073cc28478a3
Abby Nyakara <abbynyakara@gmail.com>
2022-10-11 10:38:26 +0300

    add forms

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,8 +4,37 @@ class PostsController < ApplicationController
     @post = @user.posts
   end
 
-  def show
-    @post = Post.joins(:user).where(user: { id: params[:user_id] }).find(params[:id])
-    @comments = @post.comments
+  def new
+    Post.new
   end
+
+  # def show
+  #   @post = Post.joins(:user).where(user: { id: params[:user_id] }).find(params[:id])
+  #   @comments = @post.comments
+  # end
+
+  def show
+    @post = Post.find(params[:id])
+    @user = @post.user,
+            @comments = @post.comments
+  end
+
+  def create
+    @post_new = current_user.posts.new(post_params[:id])
+    respond_to do |format|
+      format.html do
+        if @post_new.save
+          redirect_to "/users/#{@post_new.user_id}/posts/", message: 'Success'
+        else
+          render :new, alert: 'Error occured please check!'
+        end
+      end
+    end
+  end
+
+  def post_params
+    params.require(:post).permit(:Title, :Text)
+  end
+
+  private :post_params
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'users#index'
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+    resources :posts, only: [:index, :show, :create, :new] do
+      resources :likes, only: [:create] 
+      resources :comments, only:[:create, :new]
+    end
   end
 end
